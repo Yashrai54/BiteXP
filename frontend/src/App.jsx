03f9ac5eed466c8f2a37459124dffd1ea0689736
Yash Rai <157264996+Yashrai54@users.noreply.github.com>
2025-10-18 14:44:31 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,31 @@
-import React from 'react'
-import {Route,Routes} from 'react-router-dom'
-import Home from './components/Home'
-import Signup from './components/Signup.jsx'
-import SignIn from './components/SignIn.jsx'
-import RecipeList from './components/RecipeList.jsx'
-import RecipeDetail from './components/RecipeDetail.jsx'
-import { useAppContext } from './context/UserContext.jsx'
-  import { Navigate } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Home from './components/Home';
+import Signup from './components/Signup.jsx';
+import SignIn from './components/SignIn.jsx';
+import RecipeList from './components/RecipeList.jsx';
+import RecipeDetail from './components/RecipeDetail.jsx';
+import { useAppContext } from './context/UserContext.jsx';
 
-const App = () => {
- 
 const ProtectedRoute = ({ children }) => {
-  const { user } = useAppContext();
-  if (!user) return <Navigate to="/signin" replace />;
-  return children;
+  const { user, loading } = useAppContext();
+
+  if (loading) return <div>Loading...</div>; // wait until context is ready
+  return user ? children : <Navigate to="/signin" replace />;
 };
 
-
+const App = () => {
   return (
-    <div>
-      <Routes>
-          <Route path='/' element={<ProtectedRoute><Home/></ProtectedRoute>}></Route>
-          <Route path='/signup' element={<Signup></Signup>}></Route>
-          <Route path='/signin' element={<SignIn></SignIn>}></Route>
-          <Route path='/recipe' element={<RecipeList></RecipeList>}></Route>
-          <Route path='/recipe/:id' element={<RecipeDetail></RecipeDetail>}></Route>
-      </Routes>
-    </div>
-  )
-}
+    <Routes>
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<Signup />} />
 
-export default App
+      {/* Protected routes */}
+      <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+      <Route path="/recipe" element={<ProtectedRoute><RecipeList /></ProtectedRoute>} />
+      <Route path="/recipe/:id" element={<ProtectedRoute><RecipeDetail /></ProtectedRoute>} />
+    </Routes>
+  );
+};
+
+export default App;
