46aa040e9122bb58608b5f40aa8d2acb0040e15a
Yash Rai <157264996+Yashrai54@users.noreply.github.com>
2025-10-18 13:51:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const port=process.env.PORT;
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
-app.use(cors({origin:"http://localhost:5173",methods:['GET','POST','PUT','DELETE'],credentials:true}))
+app.use(cors({origin:"https://bite-xp.vercel.app/",methods:['GET','POST','PUT','DELETE'],credentials:true}))
 app.use("/api/recipe",recipeRouter)
 app.use("/api/auth",userRouter)
 app.get("/",(req,res)=>{
